ce32e68e8283ecdabde3554627d7b5ccb9d598b9
Nguyen Van Dat <datsunbae@gmail.com>
2022-08-05 07:45:58 +0700

    style: Add React-Icons to Node Package

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import {GrEdit} from 'react-icons/gr';
+import {IoClose} from "react-icons/io5";
+
 
 function Todo(props) {
   return (
     <ul>
-        {props.todos.map((todo) => <li>{todo.contentTodo}</li>)}
+        {props.todos.map((todo, index) => 
+        <li key={index}>
+            {todo.contentTodo}
+            <GrEdit />
+            <IoClose />
+        </li>)}
     </ul>
   )
 }
